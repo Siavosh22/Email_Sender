803320c6ef010fb13a77d6e64f83354902310d6c
Siavosh22 <siavoshmozafary@gmail.com>
2022-09-05 10:52:08 +0200

    Initial commit

---
+++ Email.py
@@ -0,0 +1,24 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = "siavoshmozafari@gmail.com"
+password = "*********"
+email_receiver = "siavoshmozafary@gmail.com"
+subject = "Hava Cheto?!"
+body = """
+hava khobist?
+GiGi cheto?!
+"""
+
+em = EmailMessage()
+em["From"] = email_sender
+em["To"] = email_receiver
+em["Subject"] = subject
+em.set_content(body)
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com",465, context=context) as smtp:
+    smtp.login(email_sender,password)
+    smtp.sendmail(email_sender,email_receiver,em.as_string())
+
